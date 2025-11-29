77d7ac1101c6742eececf87ce384a1aafb1d5311
mahdi rafie <mahdi47rafie@gmail.com>
2025-11-29 11:58:44 +0330

    removed dist

--- dist/config/database.js
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from "sequelize";
-const sequelize = new Sequelize('basu118', 'basu', 'basudb', {
-    host: 'localhost',
-    dialect: 'mysql',
-    logging: true
-});
-export default sequelize;
-//# sourceMappingURL=database.js.map
